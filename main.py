a3dfb7114a48407bc24aefd932e241df3f7342d1
abner <abnerbessi@gmail.com>
2022-05-02 11:51:04 -0300

    add request enpoint

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from requests.structures import CaseInsensitiveDict
 
 
 def call():
-    # Use a breakpoint in the code line below to debug your script.
-    print("Oracle BI Publisher")  # Press ⌘F8 to toggle the breakpoint.
-    print("Executando")
+    url = "http://osbdev.odebrecht.com/OdbSOA-NotificationManager/NotificationManager2Port";
+    headers = CaseInsensitiveDict()
+    headers = {'content-type': 'text/xml'}
+    payload="""
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soa="http://www.odebrecht.com/soa">
+           <soapenv:Header/>
+           <soapenv:Body>
+              <soa:getSoaInterface>
+                 <!--Optional:-->
+                 <eventName>xxod.psft_classif_cont_mwd_ret</eventName>
+                 <!--eventName>xxod.mwd_xml_Lamborghini_dfe</eventName-->
+              </soa:getSoaInterface>
+           </soapenv:Body>
+        </soapenv:Envelope>    
+    """;
+
+    resp = requests.post(url, headers=headers, data=payload)
+
+    print(resp.text)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
