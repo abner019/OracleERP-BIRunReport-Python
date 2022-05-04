522e51c58726ec029c8378298e34e55e78ef1955
abner <abnerbessi@gmail.com>
2022-05-03 22:37:50 -0300

    add request enpoint

--- main.py
+++ main.py
@@ -3,20 +3,34 @@ from requests.structures import CaseInsensitiveDict
 
 
 def call():
-    url = "http://osbdev.odebrecht.com/OdbSOA-NotificationManager/NotificationManager2Port";
+    url = "https://efei-dev1.fa.la1.oraclecloud.com:443/xmlpserver/services/PublicReportService";
     headers = CaseInsensitiveDict()
     headers = {'content-type': 'text/xml'}
     payload="""
-        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soa="http://www.odebrecht.com/soa">
-           <soapenv:Header/>
-           <soapenv:Body>
-              <soa:getSoaInterface>
-                 <!--Optional:-->
-                 <eventName>xxod.psft_classif_cont_mwd_ret</eventName>
-                 <!--eventName>xxod.mwd_xml_Lamborghini_dfe</eventName-->
-              </soa:getSoaInterface>
-           </soapenv:Body>
-        </soapenv:Envelope>    
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:pub="http://xmlns.oracle.com/oxp/service/PublicReportService">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <pub:runReport>
+         <pub:reportRequest>
+        
+            <pub:parameterNameValues>
+               <!--Zero or more repetitions:-->
+               <pub:item>         
+                  <pub:name>p_invoice_id</pub:name>
+                  <pub:values>
+                     <!--Zero or more repetitions:-->
+                     <pub:item>300000021159041</pub:item>
+                  </pub:values>
+               </pub:item>
+            </pub:parameterNameValues>
+            <pub:reportAbsolutePath>/Custom/Integration/treinamento/abg/queryInvoicesRPT.xdo</pub:reportAbsolutePath>
+            <pub:sizeOfDataChunkDownload>-1</pub:sizeOfDataChunkDownload>
+         </pub:reportRequest>
+         <pub:userID>INT_TREINAMENTO</pub:userID>
+         <pub:password>Oracle123</pub:password>
+      </pub:runReport>
+   </soapenv:Body>
+</soapenv:Envelope>
     """;
 
     resp = requests.post(url, headers=headers, data=payload)
